60fe0994d04b2d677f23b90be2f7df8d2475764b
Arturo Valverde <art.valverde.563@gmail.com>
2025-12-04 20:57:42 -0600

    Feat: Added page  404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+get_header();
+?>
+
+<main class="404 bg-bicolor">
+
+    <section class="container">
+
+        <p class="h2 tx-center">404:</p>
+        <p class="h2 tx-center">Page Not Found</p>
+
+    </section>
+
+</main>
+
+<?php get_footer() ?>
