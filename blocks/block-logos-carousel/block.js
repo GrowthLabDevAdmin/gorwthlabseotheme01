399fd155d7256465cf298e661daa226b65525208
ArturoValv <art.valverde.563@gmail.com>
2026-01-16 23:00:03 -0600

    style: changes at logos carousel slide script

--- blocks/block-logos-carousel/block.js
+++ blocks/block-logos-carousel/block.js
@@ -7,15 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
       new Splide(logosCarousels[i], {
         type: "loop",
         perPage: 7,
-        perMove: 1,
+        perMove: 7,
         arrows: false,
         pagination: true,
         breakpoints: {
           [tablet]: {
             perPage: 3,
+            perMove: 3,
           },
           [ldpi]: {
             perPage: 5,
+            perMove: 5,
           },
         },
       }).mount();
